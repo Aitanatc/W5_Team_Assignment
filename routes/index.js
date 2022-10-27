4cdac48a1496a23f5cadbad4b2ae516420bcd358
Aitanantc <aitanantc@gmail.com>
2022-10-26 22:47:25 -0500

    changes to deploy correctly

--- routes/index.js
+++ routes/index.js
@@ -4,13 +4,13 @@ const express = require('express');
 const router = express.Router();
 
 router.use('/', require('./swagger'));
-// router.use('/task', require('./task'));
+router.use('/task', require('./task'));
 
-router.use('/task', (req, res) => {
-    res.redirect(
-        `https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`,
-    );
-});
+// router.use('/task', (req, res) => {
+//     res.redirect(
+//         `https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`,
+//     );
+// });
 
 router.use('/auth', (req, res) => {
     res.redirect(
@@ -20,8 +20,8 @@ router.use('/auth', (req, res) => {
 
 router.use('/auth-callback', (req, res) => {
     res.redirect(
-        // "https://tastsmanager.onrender.com/api-docs/",
-        "http://localhost:3000/api-docs",
+        "https://tastsmanager.onrender.com/api-docs/",
+        // "http://localhost:3000/api-docs",
 
     );
      
